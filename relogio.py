19d4ca9824a014ba111ae96d2b2d9478bc4d5959
ChrystianMyke <miyamoto13k@gmail.com>
2023-11-18 16:15:27 -0300

    first commit

---
+++ relogio.py
@@ -0,0 +1,28 @@
+from tkinter import *
+from time import strftime
+
+# Função para atualizar o relógio
+def atualizar_relogio():
+    horario_atual = strftime("%H:%M:%S %p")
+    rotulo_relogio.config(text=horario_atual)
+    rotulo_relogio.after(1000, atualizar_relogio)
+    
+# Criação da janela principal
+janela = Tk()
+janela.title('Relógio Digital Simples')
+
+# Criação do rótulo para exibir o relógio
+rotulo_relogio = Label(
+    janela,
+    font=('Arial',40,'bold'),
+    background='light green',
+    foreground='black'
+)
+#Posiciona o rótulo no centro da janela
+rotulo_relogio.pack(anchor='center')
+
+# inicia a atualização do relógio
+atualizar_relogio()
+
+#Inicia o loop principal da interface gráfica
+janela.mainloop()
